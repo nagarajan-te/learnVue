ba5107de47e87248f006088cb695469297a7a80e
Nagarajan <nagarajan@thousandeyes.com>
2020-05-09 22:33:33 -0700

    add the style file and the product image url

--- introToVue/main.js
+++ introToVue/main.js
@@ -2,7 +2,8 @@ let app = new Vue({
     el: '#app',
     data: {
         product: {
-            title: 'Super Duper Socks'
+            title: 'Super Duper Socks',
+            description: 'Anyone wearing them immediately gains duper powers.'
         }
     }
 })
